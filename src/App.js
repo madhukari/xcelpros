3f84e21e46523fa438fb97ccc927f9007192acd9
madhukiranka <62378655+madhukiranka@users.noreply.github.com>
2020-07-19 00:03:12 +0530

    Added React Table with movable cols&rows  added styling using sass scss, added headers menu bars icons using html5&material UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { useState } from 'react';
+import ReactTableDragColumnRow from './Components/Table';
+
+
+
 
 function App() {
+  let [data, setData] = useState({
+    heads: ["Things to do", "Owner", "Status", "Duedate","priority", "+"],
+    rows: [
+      ["new", "stuck",3, 1, 2, 3],
+      [11, 2, 3, 11, 2, 3],
+      [1, 22, 3, 1, 22, 3],
+      [1, 2, 33, 1, 2, 33]
+    ]
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+
+       <div className="App-sideNav">
+      {/* <img src='/public/images/perm' */}
+      <img src="/perm_identity-black-18dp.svg" alt="image" />
+
+       </div>
+       <div className="App-sideNav2">
+         <h3 className="alignleft"> Work Spaces</h3>
+{/* 
+         <div id="fakebox">        
+        <input id="fakebox-input" autocomplete="off" tabindex="-1" type="url" aria-hidden="true"></input>
+        
+      </div> */}
+
+      <div className="divInsideNav">
+      <h5 className="alignleft"> main</h5>
+
+      {/* <img src="C:\Users\Lenovo\Downloads\eject-black-18dp" alt="image" /> */}
+      </div>
+       </div>
+     
+       <div>
+       <div className="App-divTop">
+         <h3 className="alignleft">Web design</h3>
+
+<h5 className="alignleft">Main Table</h5>
+         
+      {/* <img src='/public/images/perm' */}
+      {/* <img src="/perm_identity-black-18dp.svg" alt="image" /> */}
+
+       </div> 
+      <ReactTableDragColumnRow
+        heads={data.heads}
+        
+        rows={data.rows}
+        onDragEnd={(type, from, to, newData) => {
+          console.log({
+            type,
+            from,
+            to,
+            newData
+          });
+          setData(newData);
+        }}
+      />
+    </div>
+
     </div>
   );
 }
